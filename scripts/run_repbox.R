2c63260630fad78e30bde9aba8a46349128dafc4
skranz-auto <xsx50@uni-ulm.de>
2023-02-13 12:24:14 +0000

    update

--- scripts/run_repbox.R
+++ scripts/run_repbox.R
@@ -40,6 +40,6 @@ system("chmod -R 777 /root/projects")
 cat("\nStore results as 7z")
 dir.create("/root/output")
 key = Sys.getenv("REPBOX_PKG_KEY")
-to.7z("/root/projects","/root/output/project.7z",password = key)
+to.7z("/root/projects/project","/root/output/project.7z",password = key)
 
 cat(paste0("\nAnalysis finished after ", round(difftime(Sys.time(),start.time, units="mins"),1)," minutes.\n"))
