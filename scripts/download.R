94fe79108172832b1a0984889e2960d3325ea344
skranz-auto <xsx50@uni-ulm.de>
2023-02-15 12:28:59 +0000

    ecta_90_2_1

--- scripts/download.R
+++ scripts/download.R
@@ -1,8 +1,10 @@
 config = yaml::yaml.load_file("/root/repbox_config.yml")
 repo_type = config$repo_type
 
+cat("\nRepotype = ", repo_type,"\n")
 if (repo_type == "zip_url") {
   url = config$url
+  cat("\nDownload supplement from ", url,"\n")
   download.file(url, "/root/zip/supplement.zip")
 } else if (repo_type == "oi") {
   source("~/scripts/download_oi.R")
